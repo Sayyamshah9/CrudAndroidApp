2f94eb6d4683904ec99fb8bbf09e960803eee7bc
Sayyam <sayyammshah@gmail.com>
2022-01-02 12:25:12 +0530

    Getting Crud Data from Api And Displaying it in Recycler View - Comit-2

--- app/src/main/java/com/example/crud/MainActivity.java
+++ app/src/main/java/com/example/crud/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.crud;
 
 import static android.content.ContentValues.TAG;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
@@ -14,6 +15,7 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import java.util.List;
@@ -69,7 +71,7 @@ public class MainActivity extends AppCompatActivity {
         //CALLING CRUD API
 //        Toast.makeText(MainActivity.this, userId, Toast.LENGTH_SHORT).show();
         String userId = sessionManager.pref.getString("USER_ID", "NULL");
-        getCrudDataAndSetInRecyclerView(userId);
+//        getCrudDataAndSetInRecyclerView(userId);
     }
 
     public  void getCrudDataAndSetInRecyclerView(String Uid){
@@ -79,24 +81,20 @@ public class MainActivity extends AppCompatActivity {
         Call<List<CrudModel>> crudCall = apiInterface1.getCrudData(Uid);
         crudCall.enqueue(new Callback<List<CrudModel>>() {
             @Override
-            public void onResponse(Call<List<CrudModel>> call, Response<List<CrudModel>> response) {
-
+            public void onResponse(@NonNull Call<List<CrudModel>> call, @NonNull Response<List<CrudModel>> response) {
                 if(response.isSuccessful()){
                     List<CrudModel> data = response.body();
                     //RECYCLERVIEW CODE
                     recyclerView = findViewById(R.id.tasklist);
                     recyclerView.setLayoutManager(new LinearLayoutManager(MainActivity.this));
-                    CrudAdapter crudAdapter =new CrudAdapter(MainActivity.this, data);
+                    CrudAdapter crudAdapter = new CrudAdapter(MainActivity.this, data);
                     recyclerView.setAdapter(crudAdapter);
-//                    for (CrudModel data1:data) {
-//                        Toast.makeText(MainActivity.this, data1.getTitle().toString(), Toast.LENGTH_SHORT).show();
-//                    }
                 }else {
                     Log.e(TAG, response.message());
                 }
             }
             @Override
-            public void onFailure(Call<List<CrudModel>> call, Throwable t) {
+            public void onFailure(@NonNull Call<List<CrudModel>> call, @NonNull Throwable t) {
                 Toast.makeText(MainActivity.this, t.getMessage(), Toast.LENGTH_SHORT).show();
             }
         });
